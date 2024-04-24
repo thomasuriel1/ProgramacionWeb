14a495eee96ab1eeeef95f8947f3d97454282884
thomas uriel <tlauravargas@gmail.com>
2024-04-24 15:38:38 -0300

    termine con el trabajo

---
+++ javascrip/bucle.js
@@ -0,0 +1,9 @@
+const arrays = (1,2,3)
+let contador = 0;
+while (contador <2){
+    console.log("hola")
+    contador++;
+}
+for(let i = 0;i< arrays, length;i++){
+    console.log("arrays",arrays[1]);
+}
---
+++ javascrip/variables.js
@@ -0,0 +1,6 @@
+var name = "hola"
+var number = 20 
+var boolean = true 
+function funcion() {
+    let name = "hola"
+}
