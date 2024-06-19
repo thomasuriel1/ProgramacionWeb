b19162d78dc281b31874c19f895022b7a9cbd9a1
“thomasuriel1” <tlauravargas@gmail.com>
2024-06-19 16:55:52 -0300

    hicimos el react

---
+++ React/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { Prueba } from "./components/Prueba";
+
+function App() {
+  return (
+    <>
+      Hola mundo
+      <Prueba nombre={"Thomas"} apellido={"Laura"} />
+    </>
+  );
+}
+
+export default App;
