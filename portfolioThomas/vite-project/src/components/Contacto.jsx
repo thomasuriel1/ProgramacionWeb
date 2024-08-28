d968611649dd95a08b8d804bd6ead71f4f073d4e
“thomasuriel1” <tlauravargas@gmail.com>
2024-08-28 16:46:15 -0300

    personalize mas el portfolio

--- portfolioThomas/vite-project/src/components/Contacto.jsx
+++ portfolioThomas/vite-project/src/components/Contacto.jsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import {FaEnvelope} from "react-icons/fa";
 
 function Contacto() {
   return (
@@ -8,7 +9,9 @@ function Contacto() {
       <h3>
         Si deseas ponerte en contacto conmigo, envíame un mensaje a
         tlauravargas@gmail.com. 
+        
       </h3>
+      <FaEnvelope className="footer-icon envelope" />
     </div>
   );
 }
--- portfolioThomas/vite-project/src/components/Projects.jsx
+++ portfolioThomas/vite-project/src/components/Projects.jsx
@@ -7,9 +7,11 @@ function Projects() {
       <h1>Mis Proyectos</h1>
       <h3>Aquí puedes ver algunos de los proyectos en los que he trabajado.</h3>
       <h4>Un conversor de monedas.</h4>
+      <img className="achicarMon" src="cap1.png"/>
       <h4>6 cartas de Rick and Morty con react.</h4>
-      <h4>9 cartas de distintas peliculas.</h4>
+      <img className="achicarNormal" src="cap3.png"/>
       <h4>9 cartas de pokemon.</h4>
+      <img className="achicarNormal" src="cap2.png"/>
       <h4>Y diversos trabajos con logica de javascript.</h4>
     </div>
   );
